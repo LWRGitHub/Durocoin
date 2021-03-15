360f474585057e672461dacfb8af3ab3ebcfc082
LWRGitHub <LoganReynoldsLWR@gmail.com>
2021-03-15 14:33:42 -0700

    Initial Commit

---
+++ Durocoin/DurocoinApp.swift
@@ -0,0 +1,17 @@
+//
+//  DurocoinApp.swift
+//  Durocoin
+//
+//  Created by Logan Reynolds on 3/15/21.
+//
+
+import SwiftUI
+
+@main
+struct DurocoinApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
